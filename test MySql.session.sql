33fb1a0aa753a3d7321ab83f58927c8ea37ef772
Kuanyshbek.M <Kuanyshbek.M@kazteleport.kz>
2024-06-04 12:01:56 +0500

    hanging dbs

--- test MySql.session.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-  id INT PRIMARY KEY,
-  name VARCHAR(255),
-  email VARCHAR(255)
-);
-
-INSERT INTO users (id, name, email) VALUES
-  (1, 'John Doe', 'johndoe@example.com'),
-  (2, 'Jane Doe', 'janedoe@example.com');
